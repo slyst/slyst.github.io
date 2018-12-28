620c61ec23b85cbdbf1db69f02261b2baf5522da
MS <msameer0106@gmail.com>
2018-12-27 21:35:08 -0500

    v1

---
+++ responses.js
@@ -0,0 +1,62 @@
+var responses = {
+    _L: {
+        n: 7118,
+        aka: 'Elle, by-defintion',
+        message: 'He\'s a nice/easy person to be around, good personality, good interests\nHe\'s also a bit smexy when he dresses as a gentleman, but most are\nProbably best is his humor and his balance with seriousness, without being extreme in either direction'
+    },
+    bedcloud: {
+        n: 6085,
+        aka: 'bedcloud',
+        message: 'Pranks and mischief, humor and hilarity. Fuzzy face, backs up his friends when other people are being asses to his pals. Great story teller GREG THE GOAT FUCKER! SPONGEBOB! SOME PEOPLE SHOULD NOT OWN PETS Someone you\'re comfortable around to do fake British, Scottish, Irish, Jamaican accents around. Very inclusive kid.\nProbably the friend you want when things get hard, or you feel dispirited.'
+    },
+    DefinitelyHuman003: {
+        n: 8105,
+        aka: 'Phantom',
+        message: 'His Maculine body\nand his big dick\ni don\'t know, maybe his not autistic way of communicating?'
+    },
+    Emue: {
+        n: 6594,
+        aka: 'Carbonara',
+        message: 'I like the fact that he eats dirt because he is a racoon\nSeems like a chill guy'
+    },
+    Esc: {
+        n: 1838,
+        aka: 'trash bird, pengwing, oh mann',
+        message: ''
+    },
+    George29: {
+        n: 5931,
+        aka: 'George, cow',
+        message: 'He is very creative and he is not showing off.'
+    },
+    'Jichael Mackson': {
+        n: 0258,
+        aka: 'Jichael',
+        message: 'Friendly enough, probably human'
+    },
+    kvon: {
+        n: 8797,
+        aka: 'Kevin',
+        message: 'desturu is da bomb'
+    },
+    Luis: {
+        n: 1690,
+        aka: 'Antmann',
+        message: 'He is funny, he likes interesting things like black holes and cool engines, his sense of humor is amazing, he is good at math, he is kind and just generally a nice person to be around.'
+    },
+    olta8: {
+        n: 2362,
+        aka: 'Olta',
+        message: 'Generally cares about the little things. Like if you mentioned an event, he would ask about it later when you would expect most people to have forgotten.'
+    },
+    POOPling: {
+        n: 9524,
+        aka: 'Poopling',
+        message: 'He\'s a sweet childish guy with moron sense of humour that i adore....\nAnd also he\'s not a total shitposter like some people(me) here in health span\nHe actually helps when the time is there to'
+    },
+    'sheep (1)': {
+        n: 3011,
+        aka: 'hi sheep',
+        message: 'when  i agree on sometihng in my mind i often say that\'s true and every time i say that i get myself reminded of Desturu\nnow i am not saying i like that necessarily. i have a mixed feeling about it. That\'s about it\nbut one more thing is desturu is a compassionate being. He\'s a good guy'
+    }
+};
